f607394bc921b72435f9a618b9c10ea03da5ae30
TikeMate <example@example.org>
2021-04-27 23:27:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# TikTok X-Gorgon & X-Khronos Generation Algorithm
+
+X-Gorgon and X-Khronos headers are required to call tiktok api. I will provide you API as rental or source with support.
+
+### Request
+```shell
+curl --request POST \
+  --url example.com/ \
+  --header 'content-type: application/json' \
+  --data '{
+   "url":"https://api2.musical.ly/aweme/v1/aweme/detail/",
+   "query":"aweme_id=6948740634060639493&os_api=25&device_type=SM-G930F&ssmix=a&manifest_version_code=2019091803&dpi=560&carrier_region=FR&uoo=0&region=US&carrier_region_v2=208&app_name=musical_ly&version_name=13.1.3&timezone_offset=3600&ts=1569407751&ab_version=13.1.3&pass-route=1&pass-region=1&is_my_cn=0&ac2=wifi5g&ac=wifi&app_type=normal&channel=googleplay&update_version_code=2019091803&_rticket=1569407751490&device_platform=android&iid=6740283443298715398&build_number=13.1.3&locale=en&version_code=130103&timezone_name=Europe%2FParis&openudid=d65d0efd4bbd2ae8&device_id=6740283146010527238&sys_region=US&app_language=en&resolution=1440*2560&device_brand=samsung&language=en&os_version=7.1.2&aid=1233&mcc_mnc=20801"
+}
+```
+### Response
+```json
+{
+  "X-Gorgon": "0404d0e400010f39aa7f1704a7760331421ccf11c4f2ad43546b",
+  "X-Khronos": "1570908582"
+}
+```
+
+
+
+## X-Gorgon & X-Khronos  used in below API
+
+1. login to the user by email and username
+2. register new device
+3. receive information of the user through user_id
+4. receive information about your authorized profile
+5. get list of user's subscribers
+6. get list of user's subscriptions
+7. receive user posts (publications)
+8. receive comments for posts
+9. like comments
+10. like posts
+11. subscribe to user
+12. search
+13. search hashtags
+14. search post hashtags
+15. search users
+16. Download Post By Video Id
+17. Download tiktok video without watermark
+
+
+
+
+
+**Contact for details:** https://t.me/meandmyrule
+
---
+++ fun.py
@@ -0,0 +1,71 @@
+
+def mo24495a(s, m, sec):
+    f54574a = "0123456789abcdef"
+
+
+
+    def m64164a(b):
+        return m64165a(b, 0, len(b))
+
+    def m64163a(s):
+        md5 = hashlib.md5()
+        md5.update(s.encode("utf-8"))
+        return m64164a(md5.hexdigest().encode("utf-8"))
+
+    def m135437e(s):
+        for str2 in s.replace(" ", "").split(","):
+            indexOf = str2.find("sessionid=")
+            if indexOf != -1:
+                return str2[indexOf + 10:]
+        return None
+
+    def m135436d(s):
+        indexOf  = s.find("?")
+        indexOf2 = s.find("#")
+        if indexOf == -1: return None
+        if indexOf2 == -1: return s[indexOf+1:]
+        if indexOf2 < indexOf: return None
+        return s[indexOf+1:indexOf2]
+
+    def m64150a(bArr):
+        n = len(bArr)
+        cArr = ["\00"] * (n * 2)
+        for i in range(n):
+            b = bArr[i] & 225
+            i2 = i * 2
+            cArr[i2] = f54574a[b >> 4]
+            cArr[i2 + 1] = f54574a[b & 15]
+        return "".join(cArr)
+
+    def m64151a(s):
+        n = len(s)
+        bArr = [0] * (n//2)
+        for i in range(0,n,2):
+            bArr[i//2] = (int(s[i], 16) << 4) + int(s[i+1], 16)
+        return bArr
+
+    b = m135436d(s)
+    str3 = None
+    a = None if b is None or len(b) == 0 else m64163a(b)
+    str4 = None
+    str5 = None
+    for k,v in m.items():
+        if k.upper() == "X-SS-STUB":
+            str3 = v
+        if k.upper() == "COOKIE":
+            str6 = v
+            if str6 is not None:
+                str4 = m64163a(str6)
+                c = m135437e(str6)
+                if c != None and len(c) > 0:
+                    str5 = m64163a(c)
+
+
+
+    if a is None or len(a) == 0: a = "00000000000000000000000000000000"
+    if str3 is None or len(str3) == 0: str3 = "00000000000000000000000000000000"
+    if str4 is None or len(str4) == 0: str4 = "00000000000000000000000000000000"
+    if str5 is None or len(str5) == 0: str5 = "00000000000000000000000000000000"
+    sb =  str3 + str4 + str5
+    a2 = m64150a(f1(sec, bytearray(m64151a(sb))))
+    return {"X-Khronos" : str(sec), "X-Gorgon" : a2}
